5a0e1c1598f15e6e037ace090cc1cf01b41402e4
georgenizam <qwqw37137@gmail.com>
2022-07-03 18:26:37 +0300

    add BaseIcon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+      :class="$attrs.class || 'w-6 h-6'"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      v-html="path"
+  >
+  </svg>
+</template>
+
+<script>
+import icons from "../icons"
+
+export default {
+  name: "BaseIcon",
+  props: ['name'],
+  data() {
+    return {
+      path: icons[this.name]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
