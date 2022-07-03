179e5590ac90c8034ea2c2707cf248e5faa8b08f
georgenizam <qwqw37137@gmail.com>
2022-07-03 21:40:03 +0300

    add DropdownAppsListItem

---
+++ src/components/DropdownAppsListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <a
+        href="#"
+        class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
+    >
+      <BaseIcon
+          name="play"
+          class="w-6 h-6 mr-3 text-red-500"
+      />
+      <span>{{ label }}</span>
+    </a>
+  </li>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+  name: "DropdownAppsListItem",
+  props: ['label'],
+  components: {
+    BaseIcon
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
