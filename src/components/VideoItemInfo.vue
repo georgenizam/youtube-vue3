4f781cd3b4abac67eee6906a252c173407d3fff6
georgenizam <qwqw37137@gmail.com>
2022-07-04 19:34:10 +0300

    add video thumbnail and video info

---
+++ src/components/VideoItemInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex items-start mt-3">
+    <img
+        :src="`https://picsum.photos/seed/avatar${index}/68`"
+        class="mr-3 rounded-full w-9 h-9"
+        alt=""
+    />
+    <div class="text-sm">
+      <span class="font-semibold text-gray-800">Video title {{ index }}</span>
+      <div class="mt-1 flex items-center">
+        <span>Channel name</span>
+        <BaseIcon
+            class="w-3.5 h-3.5 ml-1"
+            name="checkCircle"
+        />
+      </div>
+      <div>
+        {{ index }}K views &middot; {{ index }} day ago
+      </div>
+    </div>
+    <button
+        class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
+    >
+      <BaseIcon
+          class="w-6 h-6"
+          name="dotsVertical"
+      />
+    </button>
+  </div>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default {
+  name: "VideoItemInfo",
+  props: ['index'],
+  components: {
+    BaseIcon
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
