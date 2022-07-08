addca2feb5cd51f10e24cacc360cec5f5f04e1a2
georgenizam <qwqw37137@gmail.com>
2022-07-08 23:44:30 +0300

    feat(compact sidebar): add nav items

---
+++ src/components/SidebarCompactNavItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <a
+        href="#"
+        :class="classes"
+    >
+      <BaseIcon
+          :name="icon"
+          class="w-6 h-6 mb-1.5"
+      />
+      <span class="text-xs">{{ label }}</span>
+    </a>
+  </li>
+</template>
+
+<script>
+import BaseIcon from "./BaseIcon.vue"
+
+export default {
+  name: "SidebarCompactNavItem",
+  props: {
+    label: String,
+    icon: String,
+    isActive: Boolean
+  },
+  components: {
+    BaseIcon
+  },
+  computed: {
+    classes() {
+      const classes = [
+        'flex',
+        'flex-col',
+        'items-center',
+        'px-2',
+        'py-5',
+        'hover:bg-gray-100'
+      ]
+
+      return this.isActive ? [...classes, 'text-red-500'] : classes
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
